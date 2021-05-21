70ca68618182de10e60fe015d259685f063c8e4a
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-05-21 11:16:52 +0700

    buku - mengisi seeder

--- database/seeders/BukusTableSeeder.php
+++ database/seeders/BukusTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BukusTableSeeder extends Seeder
 {
@@ -13,6 +14,49 @@ class BukusTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('anggota')->insert([
+            [
+                'id'  			=> 1,
+                'judul'  		=> 'Machine Learning Tingkat Dasar dan Lanjut',
+                'isbn'			=> '9786026232786',
+                'pengarang' 	=> 'Suyanto',
+                'penerbit'		=> 'PT. Informatika',
+                'tahun_terbit'	=> 2018,
+                'jumlah_buku'	=> 5,
+                'deskripsi'		=> 'Buku mengambarkan holistik dan simpel mengenai konsep dasar machine learning',
+                'lokasi'		=> 'rak1',
+                'cover'			=> 'buku1.jpg',
+                'created_at'    => \Carbon\Carbon::now(),
+                'updated_at'    => \Carbon\Carbon::now()
+              ],
+              [
+                'id'  			=> 2,
+                'judul'  		=> 'ANDROID Pemrograman Aplikasi Mobile Berbasis Android',
+                'isbn'			=> '9786026232939',
+                'pengarang' 	=> 'Nazruddin Safaat H',
+                'penerbit'		=> 'PT. Restu Guru',
+                'tahun_terbit'	=> 2019,
+                'jumlah_buku'	=> 9,
+                'deskripsi'		=> 'Buku untuk Orang Yang Berminat Menjadi Developer Atau Pengembang Aplikasi Berbasis android',
+                'lokasi'		=> 'rak3',
+                'cover'			=> 'buku2.jpg',
+                'created_at'    => \Carbon\Carbon::now(),
+                'updated_at'    => \Carbon\Carbon::now()
+              ],
+              [
+                'id'  			=> 3,
+                'judul'  		=> 'Framework Laravel 6',
+                'isbn'			=> '9786237156048',
+                'pengarang' 	=> 'Anton Subagia',
+                'penerbit'		=> 'CV. Asfa Media',
+                'tahun_terbit'	=> 2020,
+                'jumlah_buku'	=> 8,
+                'deskripsi'		=> 'Buku Pertama Belajar Framework Laravel untuk Pemula',
+                'lokasi'		=> 'rak2',
+                'cover'			=> 'buku3.jpg',
+                'created_at'    => \Carbon\Carbon::now(),
+                'updated_at'    => \Carbon\Carbon::now()
+              ],
+        ]);
     }
 }

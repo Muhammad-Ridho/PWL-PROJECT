afdcca33e201b348edb1952a625af87498c2e196
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-05-21 10:49:20 +0700

    user - mengisi seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,31 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('Users')->insert([
+            [
+                'id'  			=> 1,
+                'name'  			=> 'Admin',
+                'username'		=> 'admin',
+                'email' 			=> 'admin@perpus.pwl',
+                'password'		=> bcrypt('admin'),
+                'gambar'			=> NULL,
+                'level'			=> 'admin',
+                'remember_token'	=> NULL,
+                'created_at'      => \Carbon\Carbon::now(),
+                'updated_at'      => \Carbon\Carbon::now()
+              ],
+              [
+                'id'  			=> 2,
+                'name'  			=> 'User1',
+                'username'		=> 'user1',
+                'email' 			=> 'user1@perpus.pwl',
+                'password'		=> bcrypt('user1'),
+                'gambar'			=> NULL,
+                'level'			=> 'user',
+                'remember_token'	=> NULL,
+                'created_at'      => \Carbon\Carbon::now(),
+                'updated_at'      => \Carbon\Carbon::now()
+              ]
+        ]);
     }
 }

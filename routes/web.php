426ea1e7b0b3925c866053e036f2c75434e32bd9
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-05-23 13:19:00 +0700

    mengatasi error di route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/', 'HomeController@index');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);

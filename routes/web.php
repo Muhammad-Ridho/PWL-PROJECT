6ffe3709f8520ef437718179a9b4fe9a5f3a4cf9
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-06-01 08:40:02 +0700

    data anggota_menambahkan controller

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
+Route::resource('anggota', 'AnggotaController');
+
 

3bbf38ef974ac13ac47c09084efa433b1de1ee92
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-06-07 19:37:07 +0700

    transaksi - link sidebar transaksi ke route transaksi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -6,6 +6,7 @@ use App\Models\Anggota;
 use App\Models\Buku;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class TransaksiController extends Controller
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -62,7 +62,7 @@
         </li>
         @endif
         <li class="nav-item">
-          <a href="#" class="nav-link">
+          <a href="{{route('transaksi.index')}}" class="nav-link">
             <i class="nav-icon fas fa-table"></i>
             <p>
               Transaksi
